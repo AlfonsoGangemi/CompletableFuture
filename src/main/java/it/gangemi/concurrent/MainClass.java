feb90a23f73e679b386c7f83d7abe63a8ee99d35
Alfonso Gangemi <alfonso.gangemi@cerved.com>
2018-07-31 11:39:02 +0200

    use logger

--- src/main/java/it/gangemi/concurrent/MainClass.java
+++ src/main/java/it/gangemi/concurrent/MainClass.java
@@ -1,17 +1,20 @@
 package it.gangemi.concurrent;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.logging.Logger;
 
 public class MainClass {
 
+    private static Logger logger = Logger.getLogger(MainClass.class.getName());
+
     public static void main(String[] args) {
 
-        System.out.println("START");
+        logger.fine("START");
 
         double a = 5, b = 3;
 
         CompletableFuture.supplyAsync(() -> {
-                    System.out.println("a = " + a);
+                    logger.info("a = " + a);
                     try {
                         Thread.sleep(5000);
                     } catch (InterruptedException e) {
@@ -20,11 +23,11 @@ public class MainClass {
                     return a * a;
                 }
         ).thenApply(k -> {
-            System.out.println("a^2 = " + k);
+            logger.info("a^2 = " + k);
             return k;
         }).thenCombineAsync(
                 CompletableFuture.supplyAsync(() -> {
-                            System.out.println("b = " + b);
+                            logger.info("b = " + b);
                             try {
                                 Thread.sleep(1000);
                             } catch (InterruptedException e) {
@@ -33,12 +36,12 @@ public class MainClass {
                             return b * b;
                         }
                 ).thenApply(k -> {
-                    System.out.println("b^2 = " + k);
+                    logger.info("b^2 = " + k);
                     return k;
                 }),
                 (x, y) -> {
                     double z = x + y;
-                    System.out.println("waiting... (c^2 = " + z + ")");
+                    logger.info("waiting... (c^2 = " + z + ")");
                     try {
                         Thread.sleep(3000);
                     } catch (InterruptedException e) {
@@ -46,7 +49,7 @@ public class MainClass {
                     }
                     return Math.sqrt(z);
                 }
-        ).thenAccept(k -> System.out.println("c = " + k)).join();
-        System.out.println("END");
+        ).thenAccept(k -> logger.info("c = " + k)).join();
+        logger.fine("END");
     }
 }
